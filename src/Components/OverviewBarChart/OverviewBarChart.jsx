23332723fb431e8e263309b8e04f100621694e8b
alexburnet <alexburnet03@gmail.com>
2024-07-08 13:44:40 +0100

    Implemented request to Java Machine Log API for barchart rather than hardcoded stats

--- src/Components/OverviewBarChart/OverviewBarChart.jsx
+++ src/Components/OverviewBarChart/OverviewBarChart.jsx
@@ -1,11 +1,43 @@
 import {Bar} from 'react-chartjs-2';
 import {Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, plugins, scales, Ticks} from 'chart.js';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 
 import {barChartData} from '../../DATA.js';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip);
 
 export const OverviewBarChart = () => {
+
+    const [overviewData, setOverviewData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/overview')
+            .then((response) => {
+                setOverviewData(response.data);
+                console.log(response.data.machines.numbers);
+                console.log(response.data.machines.percentRun);
+            })
+            .catch((error) => {
+                setError(error);
+            });
+    }, []);
+
+    if (overviewData === null) {
+        return <div>Loading...</div>;
+    }
+
+    const barChartData = {
+        labels: overviewData.machines.numbers,
+        datasets: [
+            {
+                data: overviewData.machines.percentRun,
+                backgroundColor: ["green", "green", "orange", "red", "red", "red", "orange", "red"],
+                borderWidth: 1,
+            },
+        ],
+    };
 
     const options = {
         responsive: true,
