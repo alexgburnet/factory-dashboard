d772fb68e289a4dec81ab1d8bbdc47b3c124acf1
alexburnet <alexburnet03@gmail.com>
2024-07-11 10:40:01 +0100

    Made machine cards more dynamic rather than hard coding machine numbers

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { NavBar } from './Components/NavBar/NavBar';
 import { OverviewBarChart } from './Components/OverviewBarChart/OverviewBarChart';
 import { MachineCard } from './Components/MachineCard/MachineCard';
 import { DateProvider } from './DateContext';
+import { MachineCardContainer } from './Components/MachineCardContainer/MachineCardContainer';
 
 function App() {
   return (
@@ -11,16 +12,7 @@ function App() {
       <div className="chart-container">
         <OverviewBarChart />
       </div>
-      <div className="machine-card-container">
-        <MachineCard machine={1}/>
-        <MachineCard machine={2}/>
-        <MachineCard machine={3}/>
-        <MachineCard machine={17}/>
-        <MachineCard machine={19}/>
-        <MachineCard machine={26}/>
-        <MachineCard machine={27}/>
-        <MachineCard machine={28}/>
-      </div>
+      <MachineCardContainer className="machine-card-container"/>
     </DateProvider>
   );
 }
