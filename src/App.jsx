8f4a897d5fb98d8e4a1c6ca838ec9552640d485f
alexburnet <alexburnet03@gmail.com>
2024-07-11 12:08:11 +0100

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import './App.css';
-import { NavBar } from './Components/NavBar/NavBar';
-import { OverviewBarChart } from './Components/OverviewBarChart/OverviewBarChart';
-import { MachineCard } from './Components/MachineCard/MachineCard';
+import { Home } from './Components/Home/Home';
+import { MachineSpecifics } from './Components/MachineSpecifics/MachineSpecifics';
 import { DateProvider } from './DateContext';
-import { MachineCardContainer } from './Components/MachineCardContainer/MachineCardContainer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <DateProvider>
-      <NavBar />
-      <div className="chart-container">
-        <OverviewBarChart />
-      </div>
-      <MachineCardContainer className="machine-card-container"/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/machine/:machineNo" element={<MachineSpecifics/>}/>
+        </Routes>
+      </Router>
     </DateProvider>
   );
 }
--- src/Components/MachineCard/MachineCard.jsx
+++ src/Components/MachineCard/MachineCard.jsx
@@ -20,7 +20,7 @@ export const MachineCard = (props) => {
     const date = formatDateToDDMMYYYY(selectedDate);
 
     useEffect(() => {
-        axios.get(`http://localhost:8080/api/machine?machineNumber=${props.machine}&date=${date}`)
+        axios.get(`http://localhost:8080/api/machineCard?machineNumber=${props.machine}&date=${date}`)
             .then((response) => {
                 if (response.data.error) {
                     setError(response.data.error);
