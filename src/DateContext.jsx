a75f7a2a1004d57e246828f9251c1f9a05ad1667
alexburnet <alexburnet03@gmail.com>
2024-07-16 09:07:26 +0100

    Start on the previous day's report

--- src/DateContext.jsx
+++ src/DateContext.jsx
@@ -2,8 +2,16 @@ import React, { createContext, useState } from "react";
 
 export const DateContext = createContext();
 
+const getPrevWorkDay = () => {
+    const today = new Date();
+    let day = today.getDay();
+    const diff = day === 0 ? 2 : (day === 1 ? 3 : 1); 
+    const prevWorkDay = new Date(today.setDate(today.getDate() - diff));
+    return prevWorkDay;
+}
+
 export const DateProvider = ({ children }) => {
-    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [selectedDate, setSelectedDate] = useState(getPrevWorkDay());
 
     return (
         <DateContext.Provider value={{ selectedDate, setSelectedDate }}>
