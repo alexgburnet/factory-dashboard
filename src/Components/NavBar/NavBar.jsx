41c1cde205ee4ae68eb7dcc5aafd3b1fffc18064
alexburnet <alexburnet03@gmail.com>
2024-08-14 13:58:56 +0100

    Rearange and re-style Nav Bar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,27 +2,19 @@ import "./NavBar.css";
 
 import React, { useState, useContext } from "react";
 import DatePicker from "react-datepicker";
-
 import { useLocation } from "react-router-dom";
-
 import "react-datepicker/dist/react-datepicker.css";
-
 import { enGB } from "date-fns/locale";
-
 import { DateContext } from "../../DateContext";
 import { FaBars, FaCog, FaHome, FaSignOutAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { DayNightToggle } from "../DayNightToggle/DayNightToggle";
-
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-
 import HamBurgerMenu from "../HamBurgerMenu/HamBurgerMenu";
-
+import { format } from 'date-fns'; // Import format from date-fns
 
 export const NavBar = () => {
-
-    const {selectedDate, setSelectedDate} = useContext(DateContext);
-
+    const { selectedDate, setSelectedDate } = useContext(DateContext);
     const location = useLocation();
 
     const getHeaderTitle = () => {
@@ -31,27 +23,25 @@ export const NavBar = () => {
         } else {
             return `Machine ${location.pathname.split("/")[2]}`;
         }
-    }
+    };
+
+    const formattedDate =format(selectedDate, 'dd/MM/yyyy');
 
     return (
         <div className="header">
-
-          <img src="/AburnetLogo.jpg" alt="ABurnet Logo" className="logo"/>
-
-          <div className="center-container">
-            <h1>{getHeaderTitle()} Overview for:</h1>
-            <DatePicker
-              className="date-picker"
-              selected={selectedDate}
-              onChange={(date) => setSelectedDate(date)}
-              dateFormat="dd/MM/yyyy"  // British date format
-              locale={enGB}            // British locale
-            />
-            <DayNightToggle />
-          </div>
-          
-          <HamBurgerMenu className="hamburger-menu"/>
-
+            <img src="/AburnetLogo.jpg" alt="ABurnet Logo" className="logo" />
+            <div className="center-container">
+                <h1>{getHeaderTitle()} Overview for</h1>
+                <DatePicker
+                    className="date-picker"
+                    selected={selectedDate}
+                    onChange={(date) => setSelectedDate(date)}
+                    dateFormat="dd/MM/yyyy"
+                    locale={enGB}
+                />
+                <DayNightToggle />
+            </div>
+            <HamBurgerMenu className="hamburger-menu" />
         </div>
-      );  
+    );
 };
