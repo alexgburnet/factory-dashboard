41e52362370e86d34591ee975f7b968dad74f093
alexburnet <alexburnet03@gmail.com>
2024-08-14 15:16:52 +0100

    Wrap app in FaultDataProvider so fault report will update when a row is removed on fault log

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,30 @@ import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
 import { DateProvider } from './DateContext';
 import { ShiftProvider } from './ShiftContext';
 import { AuthProvider } from './AuthContext';
+import { FaultDataProvider } from './FaultDataContext';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <DateProvider>
-      <ShiftProvider>
-        <AuthProvider>
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/machine/:machineNo" element={<PrivateRoute element={<MachineSpecifics />} />} />
-              <Route
-                path="/controlpanel"
-                element={<PrivateRoute element={<ControlPanel />} />}
-              />
-            </Routes>
-          </Router>
-        </AuthProvider>
-      </ShiftProvider>
-    </DateProvider>
+    <FaultDataProvider>
+      <DateProvider>
+        <ShiftProvider>
+          <AuthProvider>
+            <Router>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/machine/:machineNo" element={<PrivateRoute element={<MachineSpecifics />} />} />
+                <Route
+                  path="/controlpanel"
+                  element={<PrivateRoute element={<ControlPanel />} />}
+                />
+              </Routes>
+            </Router>
+          </AuthProvider>
+        </ShiftProvider>
+      </DateProvider>
+    </FaultDataProvider>
   );
 }
 
