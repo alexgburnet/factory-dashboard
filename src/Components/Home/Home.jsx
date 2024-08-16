c64b505ae20b2145eda75681a0f38ddd2d50c90f
alexburnet <alexburnet03@gmail.com>
2024-08-16 10:36:56 +0100

    Added Action list component to home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css';
 import { NavBar } from '../NavBar/NavBar';
 import { OverviewBarChart } from '../OverviewBarChart/OverviewBarChart';
 import { MachineCardContainer } from '../MachineCardContainer/MachineCardContainer';
+import { ActionList } from '../ActionList/ActionList';
 
 export const Home = () => {
   return (
@@ -10,6 +11,9 @@ export const Home = () => {
       <NavBar />
       <div className="chart-container">
         <OverviewBarChart />
+      </div>
+      <div className="action-list-container">
+        <ActionList />
       </div>
       <MachineCardContainer className="machine-card-container"/>
     </>
